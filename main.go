0ca0faa91c198e5239a484874ba823c82935779c
Lukas K Schumann <lukas_kilian.schumann@stud-mail.uni-wuerzburg.de>
2024-10-31 23:42:16 +0100

    field/delimiter working correctly

--- main.go
+++ main.go
@@ -8,7 +8,7 @@ import (
     "os"
     "strings"
     "flag"
-    "unicode"
+    // "unicode"
     "sort"
     "time"
 )
@@ -28,6 +28,7 @@ func str_to_time(str string) time.Time {
 type lineentry struct {
     Count int   `json:"count"`
     Line string `json:"line"`
+    FullLine string `json:"-"`
 }
 // LineEntries implements sort.Interface for []lineentry
 type LineEntries []lineentry
@@ -37,39 +38,43 @@ func (l LineEntries) Less(i, j int) bool {
     if l[i].Count < l[j].Count {
         return true
     }
-    iRunes := []rune(l[i].Line)
-    jRunes := []rune(l[j].Line)
+    return false
+    // TODO: different sorting options: alphabet, natural, etc
+    // iRunes := []rune(l[i].Line)
+    // jRunes := []rune(l[j].Line)
 
-    max := len(iRunes)
-    if max > len(jRunes) {
-        max = len(jRunes)
-    }
+    // max := len(iRunes)
+    // if max > len(jRunes) {
+    //     max = len(jRunes)
+    // }
 
-    for idx := 0; idx < max; idx++ {
-        ir := iRunes[idx]
-        jr := jRunes[idx]
+    // for idx := 0; idx < max; idx++ {
+    //     ir := iRunes[idx]
+    //     jr := jRunes[idx]
 
-        lir := unicode.ToLower(ir)
-        ljr := unicode.ToLower(jr)
+    //     lir := unicode.ToLower(ir)
+    //     ljr := unicode.ToLower(jr)
 
-        if lir != ljr {
-            return lir < ljr
-        }
+    //     if lir != ljr {
+    //         return lir < ljr
+    //     }
 
-        // the lowercase runes are the same, so compare the original
-        if ir != jr {
-            return ir < jr
-        }
-    }
+    //     // the lowercase runes are the same, so compare the original
+    //     if ir != jr {
+    //         return ir < jr
+    //     }
+    // }
 
-    // If the strings are the same up to the length of the shortest string,
-    // the shorter string comes first
-    return len(iRunes) < len(jRunes)
+    // // If the strings are the same up to the length of the shortest string,
+    // // the shorter string comes first
+    // return len(iRunes) < len(jRunes)
 }
 
 type settings struct {
     Name string `json:"name"`
     Edited string `json:"edited"`
+    Delimiter string `json:"delimiter"`
+    Field int `json:"field"`
 }
 
 type db_state struct {
@@ -77,9 +82,9 @@ type db_state struct {
     DB []lineentry  `json:"db"`
 }
 // Create new empty db_state
-func db_state_init_empty(name string) *db_state {
+func db_state_init_empty(name string, delim string, field int) *db_state {
     var new_db db_state
-    new_db.Conf = settings{name,time_to_str(time.Now())}
+    new_db.Conf = settings{Name:name,Edited:time_to_str(time.Now()),Delimiter:delim,Field:field}
     new_db.DB = make([]lineentry, 0)
     return &new_db
 }
@@ -97,10 +102,10 @@ func db_state_get_save_path(name string) string {
     return db_state_get_save_dir() + "/" + name + ".json"
 }
 // Load a db file, requires name
-func db_state_load(name string) *db_state {
+func db_state_load(name string, delim string, field int) *db_state {
     db_save_path := db_state_get_save_path(name)
 
-    new_db := db_state_init_empty(name)
+    new_db := db_state_init_empty(name, delim, field)
     // check if file exists
     file, err := os.Open(db_save_path)
     defer file.Close()
@@ -125,6 +130,11 @@ func db_state_load(name string) *db_state {
             return new_db
         }
         new_db.from_json(bytes_read[0:n])
+        if new_db.Conf.Field != field || new_db.Conf.Delimiter != delim {
+            fmt.Fprintf(os.Stderr, "Delimiter and field values from saved DB ('%s', %d) are different to passed arguments ('%s', %d)!\n",
+            new_db.Conf.Delimiter, new_db.Conf.Field, delim, field)
+            panic("Closing")
+        }
         return new_db
     }
 }
@@ -205,8 +215,6 @@ func (db *db_state) sort_asc() {
     sort.Sort(sort.Reverse(LineEntries(db.DB)))
 }
 func (db *db_state) add_stdin() {
-    // TODO: field/delim functionallity
-
     // Check if stdin is connected
     stat, _ := os.Stdin.Stat()
     if (stat.Mode() & os.ModeCharDevice) == 0 {
@@ -221,12 +229,29 @@ func (db *db_state) add_stdin() {
     text, err := reader.ReadString('\n')
     counter := 0
     for err == nil {
-        // counter += 1
-        // fmt.Print(text)
-        // str := string()
         text = strings.ReplaceAll(text, "\n", "")
-        if db._find_entry(text) == -1 {
-            db.DB = append(db.DB, lineentry{Count:counter,Line:strings.ReplaceAll(text,"\n", "")})
+        id := text
+
+        use_delim := db.Conf.Delimiter != "" && db.Conf.Field != -1
+        // Do the splits
+        if use_delim {
+            text_split := strings.Split(text, db.Conf.Delimiter)
+            if len(text_split) > db.Conf.Field {
+                id = text_split[db.Conf.Field]
+            } else {
+                id = ""
+            }
+        }
+        id = strings.ReplaceAll(id,"\n", "")
+        text = strings.ReplaceAll(text,"\n", "")
+        if id != "" {
+            // Not saved
+            if idx := db._find_entry(id); idx == -1 {
+                db.DB = append(db.DB, lineentry{Count:counter,Line:id,FullLine:text})
+            } else {
+                db.DB[idx].FullLine = text
+            }
+
         }
         text, err = reader.ReadString('\n')
     }
@@ -234,6 +259,9 @@ func (db *db_state) add_stdin() {
 
 
 func main() {
+    // TODO: lower count over time
+
+
     // help v delim^field^positional=1 v remove^positional=1 v path v positional=1
 
     argsHelp := flag.Bool("help", false, "Print this message.")
@@ -242,7 +270,7 @@ func main() {
     argsRemove := flag.String("remove", "", "Remove specified line from database.")
     argsName := flag.String("name", "", "REQUIRED! Name for the database.")
     argsPath := flag.Bool("path", false, "Print path to database save location.")
-    argsIncrement := flag.String("inc", "", "Increment the line with the specified identifier.")
+    argsIncrement := flag.String("increment", "", "Increment the line with the specified identifier.")
     flag.Parse()
     if !( *argsHelp || *argsDelim!=""&&*argsField!=-1&&*argsName!="" || *argsRemove!=""&&*argsName!="" || *argsPath || *argsName!="" ) {
         flag.Usage()
@@ -262,22 +290,14 @@ func main() {
         fmt.Printf("Path: %s\n",text)
         return
     }
-    if *argsDelim!=""&&*argsField!=-1&&*argsName!="" {
-        if *argsField < 0 {
-            fmt.Fprintln(os.Stderr, "--field must be >= 0!")
-        }
 
-        if *argsIncrement!="" {
-
-        } else {
-
-        }
-
-        fmt.Println("Use delim: name:", *argsName, "delim:", *argsDelim, "field:", *argsField)
-        return
-    }
+    // if *argsDelim!=""&&*argsField!=-1&&*argsName!="" {
+    //     if *argsField < 0 {
+    //         fmt.Fprintln(os.Stderr, "--field must be >= 0!")
+    //     }
+    // }
     if *argsRemove!=""&&*argsName!="" {
-        db := db_state_load(*argsName)
+        db := db_state_load(*argsName, *argsDelim, *argsField)
         idx:= db._find_entry(*argsRemove)
         if idx == -1 {
             fmt.Fprintf(os.Stderr, "Entry <%s> not found in DB!\n", *argsRemove)
@@ -289,7 +309,7 @@ func main() {
     }
     if *argsName!="" {
         // fmt.Println("New db with name:", *argsName)
-        db := db_state_load(*argsName)
+        db := db_state_load(*argsName, *argsDelim, *argsField)
         db.add_stdin()
         if *argsIncrement!="" {
             db.increment(*argsIncrement)
@@ -298,39 +318,33 @@ func main() {
         } else {
             db.sort_asc()
             for i := 0; i< len(db.DB); i++ {
-                fmt.Printf("%s\n", db.DB[i].Line)
+                fmt.Printf("%s\n", db.DB[i].FullLine)
             }
         }
         return
     }
 
+    // new_db := db_state_init_empty("testname")
+    // new_db.DB = append(new_db.DB, lineentry{Count:3,Line:"test-line"})
 
 
+    // fmt.Println("\nAll read!")
 
+    // for i := 0; i< len(new_db.DB); i++ {
+    //     fmt.Printf("%d: %s\n", new_db.DB[i].Count, new_db.DB[i].Line)
+    // }
+    // fmt.Println("\nNow sorted!")
+    // new_db.sort_asc()
+    // for i := 0; i< len(new_db.DB); i++ {
+    //     fmt.Printf("%d: %s\n", new_db.DB[i].Count, new_db.DB[i].Line)
+    // }
 
+    // fmt.Println("\n---")
 
-
-    new_db := db_state_init_empty("testname")
-    new_db.DB = append(new_db.DB, lineentry{Count:3,Line:"test-line"})
-
-
-    fmt.Println("\nAll read!")
-
-    for i := 0; i< len(new_db.DB); i++ {
-        fmt.Printf("%d: %s\n", new_db.DB[i].Count, new_db.DB[i].Line)
-    }
-    fmt.Println("\nNow sorted!")
-    new_db.sort_asc()
-    for i := 0; i< len(new_db.DB); i++ {
-        fmt.Printf("%d: %s\n", new_db.DB[i].Count, new_db.DB[i].Line)
-    }
-
-    fmt.Println("\n---")
-
-    // Test mkdir
-    ndb := db_state_load("a")
-    fmt.Println(ndb.to_json())
-    ndb.db_state_save()
-    ndb = db_state_load("a")
-    fmt.Println(ndb.to_json())
+    // // Test mkdir
+    // ndb := db_state_load("a")
+    // fmt.Println(ndb.to_json())
+    // ndb.db_state_save()
+    // ndb = db_state_load("a")
+    // fmt.Println(ndb.to_json())
 }
